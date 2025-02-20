21749a0cd7dc68df32c010ff8dce8ded97f09b31
Suujeet <itsmesujeet47@gmail.com>
2025-02-20 17:57:38 +0545

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main ()
+{
+	int a,b,c;
+	printf("Enter any two numbers\n");
+	scanf("%d %d",&a,&b);
+	c=a+b;
+	printf("The sum of two entered numbers is %d",c);
+	return 0;
+	getch ();
+}
--- test_multiple_divisible.c
+++ test_multiple_divisible.c
@@ -7,22 +7,22 @@ int main()
 
   if(num%5==0 &&(num%7==0 && num%11!=0))
   {
-    printf("\n %d is multiple of 5, divisible by 7 and not by 11.", num);
+    printf("\nYes, %d is multiple of 5, divisible by 7 and not by 11.", num);
   }
-  else if(num%5==0)
+  else if(num%5!=0 &&(num%7==0 && num%11!=0))
   {
-  	if(num%7!=0)
-  	{
-  		printf("ooops not divisible by 7");
-	  }
-	  else if(num%11==0)
-	  {
-	  	printf("ooops number is divisible by 11");
-	  }
+    printf("\n%d is not multiple of 5, divisible by 7 and not by 11.", num);
   }
-  else{
-  printf("not multiple of 5");
+  else if(num%5==0 &&(num%7!=0 && num%11!=0))
+  {
+    printf("\n%d is mutiple of 5 , not divisible by 7 and 11.", num);
+  }
+  else if(num%5!=0 &&(num%7!=0 && num%11==0))
+  {
+    printf("\n%d is divisible by 11 only.", num);
+  }
+  else
+  {
+    printf("\n%d is neither multiple of 5, nor divisible by 7 and 11.", num);
+  }
 }
-  }
- 
-
